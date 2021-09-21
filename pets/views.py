bc2d3e0415bd1af5a9502060ae819a58a653714f
邱竑達 <10746001@gmail.com>
2021-09-21 15:12:10 +0800

    連結修改
    圖片連結及網頁連結全部修改

--- pets/views.py
+++ pets/views.py
@@ -113,6 +113,36 @@ def delete(request, pk):
 
     return render(request, 'delete.html', context)
 
+# 散步
+def walk(request):
+    return render(request, 'pet-w0.html')
+def walk1(request):
+    return render(request, 'pet-w1.html')
+def walk2(request):
+    return render(request, 'pet-w2.html')
+def walk3(request):
+    return render(request, 'pet-w3.html')
+def walk4(request):
+    return render(request, 'pet-w4.html')
+def walk5(request):
+    return render(request, 'pet-w5.html')
+
+
+# 住宿
+def consult(request):
+    return render(request, 'pet-h.html')
+
+# 美容
+def grooming(request):
+    return render(request, 'pet-g11.html')
+def grooming1(request):
+    return render(request, 'pet-g12.html')
+
+# 訓練
+def train(request):
+    return render(request, 'pet-t11.html')
+def train1(request):
+    return render(request, 'pet-t12.html')
 
 # 領養
 def adopt(request):
@@ -136,10 +166,80 @@ def adopt5(request):
 def adopt6(request):
     return render(request, 'pet-a6.html')    
 
-# 諮詢
-def consult(request):
-    return render(request, 'pet-h.html')
+# 商城
+def s11(request):
+    return render(request, 'pet-s11.html')  
+def s12(request):
+    return render(request, 'pet-s12.html')  
+def s13(request):
+    return render(request, 'pet-s13.html')  
+
+def s21(request):
+    return render(request, 'pet-s21.html')  
+def s22(request):
+    return render(request, 'pet-s22.html')  
+def s31(request):
+    return render(request, 'pet-s31.html')  
+def s32(request):
+    return render(request, 'pet-s32.html')  
+def s33(request):
+    return render(request, 'pet-s33.html')  
+def s34(request):
+    return render(request, 'pet-s34.html')  
+def s35(request):
+    return render(request, 'pet-s35.html')  
+def s36(request):
+    return render(request, 'pet-s36.html')  
+def s37(request):
+    return render(request, 'pet-s37.html')  
+def s38(request):
+    return render(request, 'pet-s38.html')  
+def s41(request):
+    return render(request, 'pet-s41.html')  
+def s42(request):
+    return render(request, 'pet-s42.html')  
+def s43(request):
+    return render(request, 'pet-s43.html')  
+def s44(request):
+    return render(request, 'pet-s44.html')  
+
+# 寵物醫院
 
 # 諮詢
 def line(request):
     return render(request, 'pet-h1.html')
+# 看診
+def h2(request):
+    return render(request, 'pet-h2.html')
+# 門診
+def h3(request):
+    return render(request, 'pet-h3.html')
+# 健康
+def h4(request):
+    return render(request, 'pet-h4.html')
+# 地圖
+def h5(request):
+    return render(request, 'pet-h5.html')
+def h51(request):
+    return render(request, 'pet-h51.html')
+def h52(request):
+    return render(request, 'pet-h52.html')
+def h53(request):
+    return render(request, 'pet-h53.html')
+def h54(request):
+    return render(request, 'pet-h54.html')
+def h55(request):
+    return render(request, 'pet-h55.html')
+
+# 常見
+def h6(request):
+    return render(request, 'pet-h6.html')
+# 關於
+def h7(request):
+    return render(request, 'pet-h7.html')
+
+# 基本
+def QA(request):
+    return render(request, 'QA.html')
+def pr(request):
+    return render(request, 'privacy.html')
