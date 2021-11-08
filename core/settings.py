0cac5f56ca47a9dc562388c0defe3dc6ac7bc3ab
ZH6012 <10746012@ntub.edu.tw>
2021-11-08 11:35:38 +0800

    測試註冊功能

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'bootstrap4',
 ]
 
+
 LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
--- pets/forms.py
+++ pets/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 from .models import Pet, Register, Money
 from django.forms import ModelForm, DateInput
 from pets.models import Event
@@ -12,20 +12,30 @@ class PetForm(forms.ModelForm):
         model = Pet
 
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     username = forms.CharField(label="帳號")
-    user_pwd = forms.CharField(label="密碼")
-    check_password = forms.CharField(label="確認密碼")
-    user_email = forms.EmailField(label="電子郵件")
+    email = forms.EmailField(label="電子郵件")
+    password1 = forms.CharField(label="密碼")
+    password2 = forms.CharField(label="密碼確認")
 
     class Meta:
-        model = Register
-        fields = (
-            'username',
-            'user_pwd',
-            'check_password',
-            'user_email',
-        )
+        model = User
+        fields = ('username', 'password1', 'password2', 'email')
+
+# class RegisterForm(forms.ModelForm):
+#     username = forms.CharField(label="帳號")
+#     user_pwd = forms.CharField(label="密碼")
+#     check_password = forms.CharField(label="確認密碼")
+#     user_email = forms.EmailField(label="電子郵件")
+#
+#     class Meta:
+#         model = Register
+#         fields = (
+#             'username',
+#             'user_pwd',
+#             'check_password',
+#             'user_email',
+#         )
 
 
 # 記帳
