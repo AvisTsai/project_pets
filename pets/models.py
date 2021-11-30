246d40d9f1e90bba86a8ca7a7e934f8f4fc2223c
unknown <10746012@ntub.edu.tw>
2021-11-30 23:21:05 +0800

    修正calendar的def

--- pets/models.py
+++ pets/models.py
@@ -144,8 +144,8 @@ class Money(models.Model):
 class Event(models.Model):
     title = models.CharField("標題", max_length=200)
     description = models.TextField()
-    start_time = models.DateTimeField(default='請選擇時間')
-    end_time = models.DateTimeField(default='請選擇時間')
+    start_time = models.DateField(default='請選擇時間')
+    end_time = models.DateField(default='請選擇時間')
 
     def __str__(self):
         return self.title
